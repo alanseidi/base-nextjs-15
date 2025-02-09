f292eb5f5805664b3956ac77ee42dcd19516fa48
Alan Seidi <seidihashimura@gmail.com>
2025-02-09 15:31:44 -0400

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import containerQueries from "@tailwindcss/container-queries";
+import scrollbar from "tailwind-scrollbar";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [forms, typography, aspectRatio, containerQueries, scrollbar],
+} satisfies Config;
